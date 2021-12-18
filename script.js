92922a7b0d9c8acf401a785137d14fa160d2721a
Noah Kim <heanokim@gmail.com>
2021-12-18 11:13:04 -0800

    Got the add button and equals button work. Just need to add code for the operators and make sure all of them work too '

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.button');
+let displayValue = 0; // display value holder
+let previousValue = 0;
+let currentValue = 0;
+
 // ADDITION (+) function
 // const add = function (a, b) {
-//     const result = a + b;
-//     return result;
+//     displayValue[1] = '+';
 // }
 // console.log(add(1, 2)) // should return 3
 
@@ -27,19 +32,19 @@ const divide = function (a, b) {
 console.log(divide(100, 2)) // should return 50
 
 // EQUALS / OPERATE (=) function
-const equals = function (operator, a, b) {
+const equals = function (operator, previousValue, currentValue) {
     switch (operator) {
         case "+":
-            return a + b;
+            return parseInt(previousValue) + parseInt(currentValue);
             break;
         case "-":
-            return a - b;
+            return previousValue - currentValue;
             break;
         case "*":
-            return a * b;
+            return previousValue * currentValue;
             break;
         case "/":
-            return a / b;
+            return previousValue / currentValue;
             break;
         default:
             console.log("the equals function didn't work")
@@ -50,8 +55,6 @@ console.log(equals("+", 200, 12)); // should return 212
 
 // clicking on NUMBERS buttons displays the numbers in the display
 // and SAVES the value in a variable for use in the calculation
-const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('.button');
 
 const one = document.querySelector('.one');
 const two = document.querySelector('.two');
@@ -64,17 +67,23 @@ const eight = document.querySelector('.eight');
 const nine = document.querySelector('.nine');
 const zero = document.querySelector('.zero');
 
-const clear = document.querySelector('.clear');
-const add = '*';
-
-let displayValue = 0; // display value holder
+const clearButton = document.querySelector('.clear');
+const addButton = document.querySelector('.add');
+const equalsButton = document.querySelector('.equals');
 
 buttons.forEach((button) => {
     button.addEventListener('click', e => {
         switch (button) {
             case one:
-                display.textContent = parseInt(display.textContent + "1");
-                displayValue = display.textContent;
+                if (previousValue === 0) {
+                    display.textContent = parseInt(display.textContent + "1");
+                    displayValue = display.textContent;
+                    console.log('displayValue: ' + displayValue);
+                } else {
+                    display.textContent = parseInt(displayValue + "1");
+                    displayValue = display.textContent;
+                    console.log('displayValue: ' + displayValue);
+                }
                 break;
             case two:
                 two.addEventListener('click', e => {
@@ -113,9 +122,23 @@ buttons.forEach((button) => {
                 display.textContent = parseInt(display.textContent + "0");
                 displayValue = display.textContent;
                 break;
-            case clear:
+            case clearButton:
                 display.textContent = 0;
                 displayValue = 0;
+                previousValue = 0;
+                currentValue = 0;
+                break;
+            case addButton:
+                operator = "+";
+                previousValue = displayValue;
+                displayValue = 0;
+                break;
+            case equalsButton:
+                displayValue = display.textContent;
+                currentValue = displayValue;
+                displayValue = equals(operator, previousValue, currentValue);
+                display.textContent = displayValue;
+                console.log('result: ' + displayValue);
                 break;
             default:
                 console.log('button press did not work');
